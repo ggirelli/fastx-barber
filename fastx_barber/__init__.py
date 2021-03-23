319b71b81d31b50d5a698461a31b7ab767a15b57
Gabriele Girelli <gigi.ga90@gmail.com>
2021-03-23 22:09:06 +0100

    Dropped support for Python 3.6 and 3.7

--- fastx_barber/__init__.py
+++ fastx_barber/__init__.py
@@ -7,12 +7,12 @@ from fastx_barber import const, scripts
 from fastx_barber import bedio, io, scriptio, seqio
 from fastx_barber import flag, match, qual, trim
 
-from importlib.metadata import version, PackageNotFoundError
+from importlib.metadata import version
 
 try:
     __version__ = version(__name__)
-except PackageNotFoundError:
-    pass
+except Exception as e:
+    raise e
 
 __all__ = [
     "__version__",
